55114ea50d385f453f83abf51e414cfabf7172c7
ykl7 <yklal95@gmail.com>
2016-11-13 10:47:19 +0530

    aadhar card reader added, import errors

---
+++ app/src/main/java/com/darkarmy/ykl/samaritan/ScannerActivity.java
@@ -0,0 +1,54 @@
+package com.darkarmy.ykl.samaritan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by apple on 13/11/16.
+ */
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+    static final String SCAN_CONTENTS = "content";
+    static final String SCAN_FORMAT = "format";
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.setAutoFocus(true);
+        List<BarcodeFormat> barcodeFormats = new ArrayList<>();
+        barcodeFormats.add(BarcodeFormat.QR_CODE);
+        mScannerView.setFormats(barcodeFormats);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Intent intent = new Intent();
+        intent.putExtra(SCAN_CONTENTS, rawResult.getText());
+        intent.putExtra(SCAN_FORMAT, rawResult.getBarcodeFormat().toString());
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+}
